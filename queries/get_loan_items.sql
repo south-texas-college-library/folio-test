62aeb7e50a858133191e28a86e165b4816c040e4
mresen19 <mresen19@southtexascollege.edu>
2025-09-22 14:17:56 -0500

    Updated columns and filter for loan items report

--- queries/get_loan_items.sql
+++ queries/get_loan_items.sql
@@ -16,7 +16,8 @@ RETURNS TABLE(
     loan_date timestamptz,
     loan_due_date timestamptz,
     loan_return_date timestamptz,
-    renewal_count integer
+    renewal_count integer,
+    material_type_name text
 )
 AS $$
     SELECT 
@@ -27,13 +28,14 @@ AS $$
         folio_derived.loans_items.loan_date,
         folio_derived.loans_items.loan_due_date,
         folio_derived.loans_items.loan_return_date,
-        folio_derived.loans_items.renewal_count
+        folio_derived.loans_items.renewal_count,
+        folio_derived.loans_items.material_type_name
     FROM folio_derived.loans_items
     WHERE 
         (item_barcode IS NULL OR folio_derived.loans_items.barcode = item_barcode)
         AND (item_status = 'All' OR folio_derived.loans_items.item_status = item_status)
         AND (folio_derived.loans_items.loan_date BETWEEN start_date AND end_date)
-        AND (folio_derived.loans_Items.patron_group_name = 'Employee')
+        AND (folio_derived.loans_items.patron_group_name = 'Student')
 $$
 LANGUAGE SQL
 STABLE

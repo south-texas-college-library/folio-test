c3f5a91738c6b669c575cf23a3fcfad59a0938b3
mresen19 <mresen19@southtexascollege.edu>
2025-09-30 12:17:20 -0500

    Updated get_loan_items.sql

--- queries/get_loan_items.sql
+++ queries/get_loan_items.sql
@@ -35,7 +35,7 @@ AS $$
         (item_barcode IS NULL OR folio_derived.loans_items.barcode = item_barcode)
         AND (item_status = 'All' OR folio_derived.loans_items.item_status = item_status)
         AND (folio_derived.loans_items.loan_date BETWEEN start_date AND end_date)
-        AND (folio_derived.loans_items.patron_group_name = 'Employee')
+        AND (folio_derived.loans_items.patron_group_name = 'Student')
 $$
 LANGUAGE SQL
 STABLE

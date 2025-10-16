4a861936a87a484cab3bde9e036d3726eeede422
mresen19 <mresen19@southtexascollege.edu>
2025-10-16 16:00:39 -0500

    Updated get_item_details.sql to remove derived tables

--- queries/get_item_details.sql
+++ queries/get_item_details.sql
@@ -37,65 +37,107 @@ RETURNS TABLE(
     subtype TEXT
 )
 AS $$
+WITH fi AS (
+	SELECT 
+		id,
+		STRING_AGG(distinct SPLIT_PART(i ->> 'value', ' : ', 1), ', ') AS identifiers
+	FROM folio_inventory.instance ins
+	CROSS JOIN LATERAL JSONB_ARRAY_ELEMENTS(ins.jsonb -> 'identifiers') AS i
+	GROUP BY
+		ins.id
+),
+fp AS (
+	SELECT
+		ins.id,
+		MAX(r[1]::text) AS pub_date
+	FROM folio_inventory.instance ins
+	CROSS JOIN LATERAL JSONB_ARRAY_ELEMENTS(ins.jsonb -> 'publication') AS p
+	CROSS JOIN LATERAL REGEXP_MATCHES(p.jsonb ->> 'dateOfPublication', '\d{4}', 'g') AS r
+	GROUP BY
+		ins.id
+),
+fn AS (
+	SELECT 
+		it.id,
+		STRING_AGG(DISTINCT n ->> 'note', ', ') FILTER (WHERE nt.jsonb ->> 'name' = 'Circulation Note') AS circulation_note,
+		STRING_AGG(DISTINCT n ->> 'note', ', ') FILTER (WHERE nt.jsonb ->> 'name' = 'Public Note') AS public_note,
+	    STRING_AGG(DISTINCT n ->> 'note', ', ') FILTER (WHERE nt.jsonb ->> 'name' = 'Staff Note') AS staff_notes,
+	    STRING_AGG(DISTINCT n ->> 'note', ', ') FILTER (WHERE nt.jsonb ->> 'name' = 'Ownership') AS ownership,
+	    STRING_AGG(DISTINCT n ->> 'note', ', ') FILTER (WHERE nt.jsonb ->> 'name' = 'Price') AS price,
+	    STRING_AGG(DISTINCT n ->> 'note', ', ') FILTER (WHERE nt.jsonb ->> 'name' = 'Inventory Date') AS inventory_date,
+	    STRING_AGG(DISTINCT n ->> 'note', ', ') FILTER (WHERE nt.jsonb ->> 'name' = 'PO Number') AS po_number,
+	    STRING_AGG(DISTINCT n ->> 'note', ', ') FILTER (WHERE nt.jsonb ->> 'name' = 'Invoice') AS invoice
+	FROM folio_inventory.item it
+	CROSS JOIN LATERAL JSONB_ARRAY_ELEMENTS(it.jsonb -> 'notes') AS n
+	LEFT JOIN folio_inventory.item_note_type nt on nt.id = (n ->> 'itemNoteTypeId')::uuid
+	GROUP BY
+		it.id
+),
+insc AS (
+	SELECT
+		id,
+		(statcodes.jsonb #>> '{}')::uuid AS code
+	FROM 
+		folio_inventory.instance AS ins
+		CROSS JOIN LATERAL JSONB_ARRAY_ELEMENTS(ins.jsonb -> 'statisticalCodeIds') AS statcodes
+),
+itsc AS (
+	select
+		id,
+		(statcodes.jsonb #>> '{}')::uuid AS code
+	FROM 
+		folio_inventory.item AS it
+		CROSS JOIN LATERAL JSONB_ARRAY_ELEMENTS(it.jsonb -> 'statisticalCodeIds') AS statcodes
+)
 SELECT
-    folio_derived.items_holdings_instances.title as title,
-    folio_derived.items_holdings_instances.call_number as call_number,
-    string_agg(DISTINCT split_part(folio_derived.instance_identifiers.identifier, ' : ', 1), ', ') AS identifier,
-	(SELECT MAX(val[1]::text) from regexp_matches(folio_derived.instance_publication.date_of_publication, '\d{4}', 'g') as val) as publication_date,
-	folio_derived.holdings_ext.permanent_location_name as holdings_permanent_location,
-    folio_derived.items_holdings_instances.barcode as item_barcode,
-    folio_derived.locations_libraries.location_name as location_name,
-    folio_derived.item_ext.material_type_name as material_type_name,
-    folio_derived.locations_service_points.service_point_name as service_point_name,
-    folio_derived.item_ext.status_name as status_name,
-    folio_derived.item_ext.permanent_loan_type_name as permanent_loan_type,
-    folio_derived.item_ext.temporary_loan_type_name as temporary_loan_type,
-    string_agg(DISTINCT folio_derived.item_notes.note, ', ') FILTER (WHERE folio_derived.item_notes.note_type_name = 'Circulation Note') AS circulation_note,
-    string_agg(DISTINCT folio_derived.item_notes.note, ', ') FILTER (WHERE folio_derived.item_notes.note_type_name = 'Public Note') AS public_note,
-    string_agg(DISTINCT folio_derived.item_notes.note, ', ') FILTER (WHERE folio_derived.item_notes.note_type_name = 'Staff Note') AS staff_notes,
-    string_agg(DISTINCT folio_derived.item_notes.note, ', ') FILTER (WHERE folio_derived.item_notes.note_type_name = 'Ownership') AS ownership,
-    string_agg(DISTINCT folio_derived.item_notes.note, ', ') FILTER (WHERE folio_derived.item_notes.note_type_name = 'Price') AS price,
-    string_agg(DISTINCT folio_derived.item_notes.note, ', ') FILTER (WHERE folio_derived.item_notes.note_type_name = 'Inventory Date') AS inventory_date,
-    string_agg(DISTINCT folio_derived.item_notes.note, ', ') FILTER (WHERE folio_derived.item_notes.note_type_name = 'PO Number') AS po_number,
-    string_agg(DISTINCT folio_derived.item_notes.note, ', ') FILTER (WHERE folio_derived.item_notes.note_type_name = 'Invoice') AS invoice,
-    folio_derived.item_statistical_codes.statistical_code_name as fund,
-    folio_derived.instance_statistical_codes.statistical_code_name as subtype
+    ins.jsonb ->> 'title' AS "Title",
+    hr.call_number AS "Call Number",
+    fi.identifiers AS "Identifiers",
+    fp.pub_date AS "Publication Date",
+    it.jsonb ->> 'barcode' AS "Barcode",
+    hl.name AS "Holdings Location",
+    il.name AS "Items Location",
+    mt.name AS "Material Type",
+    sp.name AS "Service Point",
+    it.jsonb -> 'status' ->> 'name' AS "Item Status",
+    plt.name AS "Permanent Loan Type",
+    tlt.name AS "Temporary Loan Type",
+    fn.circulation_note AS "Circulation Notes",
+    fn.public_note AS "Public Notes",
+    fn.staff_notes AS "Staff Notes",
+    fn.ownership AS "Ownership",
+    fn.price AS "Price",
+    fn.inventory_date AS "Inventory Date",
+    fn.po_number AS "PO Number",
+    fn.invoice AS "Invoice",
+    finsc.jsonb ->> 'name' AS "Subtype",
+    fitsc.jsonb ->> 'name' AS "Fund"
 FROM
-	folio_derived.items_holdings_instances
-    LEFT JOIN folio_derived.item_ext ON folio_derived.item_ext.item_id = folio_derived.items_holdings_instances.item_id
-    LEFT JOIN folio_derived.holdings_ext ON folio_derived.holdings_ext.holdings_id = folio_derived.items_holdings_instances.holdings_id
-    LEFT JOIN folio_derived.item_notes ON folio_derived.item_notes.item_id = folio_derived.items_holdings_instances.item_id
-    LEFT JOIN folio_derived.instance_identifiers ON folio_derived.instance_identifiers.instance_id = folio_derived.items_holdings_instances.instance_id
-    LEFT JOIN folio_derived.instance_publication ON folio_derived.instance_publication.instance_id = folio_derived.items_holdings_instances.instance_id
-    LEFT JOIN folio_derived.locations_libraries ON folio_derived.locations_libraries.location_id = folio_derived.item_ext.effective_location_id
-    LEFT JOIN folio_derived.locations_service_points ON folio_derived.locations_service_points.location_id = folio_derived.item_ext.effective_location_id
-    LEFT JOIN folio_derived.instance_statistical_codes ON folio_derived.instance_statistical_codes.instance_id = folio_derived.items_holdings_instances.instance_id
-    LEFT JOIN folio_derived.item_statistical_codes ON folio_derived.item_statistical_codes.item_id = folio_derived.items_holdings_instances.item_id
+	folio_inventory.instance ins
+	LEFT JOIN folio_inventory.holdings_record__t hr ON hr.instance_id = ins.id
+	LEFT JOIN folio_inventory.item it ON it.holdingsrecordid = hr.id
+	LEFT JOIN folio_inventory.loan_type__t plt ON plt.id = it.permanentloantypeid
+	LEFT JOIN folio_inventory.loan_type__t tlt ON tlt.id = it.temporaryloantypeid
+	LEFT JOIN folio_inventory.location__t hl ON hl.id = hr.permanent_location_id
+	LEFT JOIN folio_inventory.location__t il ON il.id = it.effectivelocationid
+	LEFT JOIN folio_inventory.material_type__t mt ON mt.id = it.materialtypeid::uuid
+	LEFT JOIN folio_inventory.service_point__t sp ON sp.id = hl.primary_service_point::uuid
+	LEFT JOIN fi ON fi.id = ins.id
+	LEFT JOIN fp ON fp.id = ins.id
+	LEFT JOIN fn ON fn.id = it.id
+	LEFT JOIN insc ON insc.id = ins.id
+	LEFT JOIN itsc ON itsc.id = it.id
+	LEFT JOIN folio_inventory.statistical_code finsc ON finsc.id = insc.code
+	LEFT JOIN folio_inventory.statistical_code fitsc ON fitsc.id = itsc.code
 WHERE
-	(service_point = 'All' OR folio_derived.locations_service_points.service_point_name = service_point)
-    AND (holdings_location = 'All' OR folio_derived.holdings_ext.permanent_location_name = holdings_location)
-	AND (item_location = 'All' OR folio_derived.locations_libraries.location_name = item_location)
-    AND (material_type = 'All' OR folio_derived.item_ext.material_type_name = material_type)
-	AND (item_status = 'All' OR folio_derived.item_ext.status_name = item_status)
-    AND (permanent_loan_type = 'All' OR folio_derived.item_ext.permanent_loan_type_name = permanent_loan_type)
-	AND (temporary_loan_type = 'All' OR folio_derived.item_ext.temporary_loan_type_name = temporary_loan_type)
-    AND (subtype = 'All' OR folio_derived.instance_statistical_codes.statistical_code_name = subtype)
-    AND (folio_derived.instance_publication.publication_ordinality = 1 OR folio_derived.instance_publication.publication_ordinality IS NULL)
-GROUP BY
-    folio_derived.items_holdings_instances.title,
-    folio_derived.items_holdings_instances.barcode,
-    folio_derived.items_holdings_instances.call_number,
-    folio_derived.item_ext.material_type_name,
-    folio_derived.item_ext.status_name,
-    folio_derived.holdings_ext.permanent_location_name,
-    folio_derived.locations_libraries.location_name,
-    folio_derived.locations_service_points.service_point_name,
-    folio_derived.instance_publication.date_of_publication,
-    folio_derived.item_ext.permanent_loan_type_name,
-    folio_derived.item_ext.temporary_loan_type_name,
-    folio_derived.instance_publication.publication_ordinality,
-    folio_derived.instance_statistical_codes.statistical_code_name,
-    folio_derived.item_statistical_codes.statistical_code_name
+	(service_point = 'All' OR sp.name = service_point)
+    AND (holdings_location = 'All' OR hl.name = holdings_location)
+	AND (item_location = 'All' OR il.name = item_location)
+    AND (material_type = 'All' OR mt.name = material_type)
+	AND (item_status = 'All' OR it.jsonb -> 'status' ->> 'name' = item_status)
+    AND (permanent_loan_type = 'All' OR plt.name = permanent_loan_type)
+	AND (temporary_loan_type = 'All' OR tlt.name = temporary_loan_type)
+    AND (subtype = 'All' OR finsc.jsonb ->> 'name' = subtype)
 $$
 LANGUAGE SQL
 STABLE

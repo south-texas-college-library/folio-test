a8546f72efdc7843be7fbb43b84105c173d36366
rmartinez_8739 <rmartinez_8739@southtexascollege.edu>
2025-11-07 11:21:09 -0600

    Updated date query again

--- queries/lcol_laptop_checkouts.sql
+++ queries/lcol_laptop_checkouts.sql
@@ -25,7 +25,7 @@ FROM
 WHERE
     isc.statistical_code_name  = 'Laptop' AND
     li.material_type_name  = 'DAYUSE-ITEM' AND
-    to_char(li.loan_date, 'yyyy-mm-dd') BETWEEN start_date AND end_date
+    li.loan_date BETWEEN start_date AND end_date
 GROUP BY
     li.current_item_effective_location_name
 ORDER BY

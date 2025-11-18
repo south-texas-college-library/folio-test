a50ab60445102414b4c4dc944fbd480fcd36b9ab
mresen19 <mresen19@southtexascollege.edu>
2025-11-18 08:52:19 -0600

    Updated get_hotspot_checkouts.sql

--- queries/get_hotspot_checkouts.sql
+++ queries/get_hotspot_checkouts.sql
@@ -27,61 +27,37 @@ RETURNS TABLE(
 )
 AS $$
 SELECT 
-    folio_inventory.instance__t.title as "Title",
-    folio_users.users__t.barcode as "User ID",
-    folio_derived.loans_items.loan_status as "Loan Status",
-    CONCAT(folio_derived.users_groups.user_first_name, ' ', folio_derived.users_groups.user_last_name) as "Name",
-    folio_derived.loans_items.copy_number as "Copy Number",
-    folio_derived.loans_items.item_status as "Item Status",
-    folio_derived.loans_items.loan_date as "Loan Date",
-    folio_derived.loans_items.loan_due_date as "Loan Due Date",
-    folio_inventory.item__t.barcode as "Item ID",
-    folio_derived.holdings_ext.permanent_location_name as "Home Location",
-    folio_derived.item_ext.effective_location_name as "Current Location",
-    folio_derived.locations_libraries.campus_name as "Owning Library",
-    string_agg(DISTINCT split_part(folio_derived.item_notes.note, ' : ', 1), ', ') FILTER (WHERE folio_derived.item_notes.note_type_name = 'Price') AS "Price",
-    string_agg(DISTINCT split_part(folio_derived.item_notes.note, ' : ', 1), ', ') FILTER (WHERE folio_derived.item_notes.note_type_name = 'PO Number') AS "PO Number",
-    string_agg(DISTINCT split_part(folio_derived.item_notes.note, ' : ', 1), ', ') FILTER (WHERE folio_derived.item_notes.note_type_name = 'Staff Note') AS "Staff Notes"
+    ins.jsonb ->> 'title' as "Title",
+    u.jsonb ->> 'barcode' as "User ID",
+    fl.jsonb -> 'status' ->> 'name' as "Loan Status",
+    NULLIF(CONCAT(u.jsonb -> 'personal' ->> 'firstName', ' ', u.jsonb -> 'personal' ->> 'lastName'), ' ') as "Name",
+    it.jsonb ->> 'copyNumber' as "Copy Number",
+    fl.jsonb ->> 'itemStatus' as "Item Status",
+    fl.jsonb ->> 'dueDate' as "Due Date",
+    fl.jsonb ->> 'loanDate' as "Loan Date",
+    it.jsonb ->> 'barcode' as "Barcode",
+    hl.name as "Home Location",
+    il.name as "Current Location",
+    lc.name as "Owning Library",
+    NULLIF(REGEXP_REPLACE(jsonb_path_query_array(it.jsonb, '$.notes[*] ? (@.itemNoteTypeId == "1fceb11c-7a89-49d6-8ef0-2a42c58556a2").note') #>> '{}', '[\[\]"]', '', 'g'), '') AS price,
+	NULLIF(REGEXP_REPLACE(jsonb_path_query_array(it.jsonb, '$.notes[*] ? (@.itemNoteTypeId == "5ec4ca65-aacc-4f16-aa9d-395efd89f850").note') #>> '{}', '[\[\]"]', '', 'g'), '') AS po_number,
+	NULLIF(REGEXP_REPLACE(jsonb_path_query_array(it.jsonb, '$.notes[*] ? (@.itemNoteTypeId == "86e6410d-4c8b-4853-8054-bd5e563e9760").note') #>> '{}', '[\[\]"]', '', 'g'), '') AS staff_notes
 FROM 
-    folio_inventory.instance__t
-    LEFT JOIN folio_inventory.holdings_record__t ON folio_inventory.holdings_record__t.instance_id = folio_inventory.instance__t.id
-    LEFT JOIN folio_inventory.item__t ON folio_inventory.item__t.holdings_record_id = folio_inventory.holdings_record__t.id
-    LEFT JOIN folio_source_record.marc__t ON folio_source_record.marc__t.instance_id = folio_inventory.instance__t.id
-    LEFT JOIN folio_derived.loans_items on folio_derived.loans_items.item_id = folio_inventory.item__t.id
-    LEFT JOIN folio_users.users__t on folio_users.users__t.id = folio_derived.loans_items.user_id
-    LEFT JOIN folio_derived.users_groups on folio_derived.users_groups.user_id = folio_users.users__t.id
-    LEFT JOIN folio_derived.instance_statistical_codes ON folio_derived.instance_statistical_codes.instance_id = folio_inventory.instance__t.id
-    LEFT JOIN folio_derived.holdings_ext ON folio_derived.holdings_ext.holdings_id = folio_inventory.holdings_record__t.id
-    LEFT JOIN folio_derived.item_ext ON folio_derived.item_ext.item_id = folio_inventory.item__t.id
-    LEFT JOIN folio_derived.item_notes ON folio_derived.item_notes.item_id = folio_inventory.item__t.id
-    LEFT JOIN folio_derived.locations_libraries on folio_derived.loans_items.current_item_effective_location_id = folio_derived.locations_libraries.location_id
-    LEFT JOIN folio_derived.loans_renewal_dates on folio_derived.loans_renewal_dates.loan_id = folio_derived.loans_items.loan_id
+    folio_inventory.instance ins
+    LEFT JOIN folio_inventory.holdings_record__t hr ON hr.instance_id = ins.id
+    LEFT JOIN folio_inventory.item it ON it.holdingsrecordid = hr.id
+    LEFT JOIN folio_circulation.loan fl on (fl.jsonb ->> 'itemId')::uuid = it.id
+    LEFT JOIN folio_users.users u on u.id = (fl.jsonb ->> 'userId')::uuid
+    LEFT JOIN folio_inventory.location__t hl ON hl.id = hr.permanent_location_id
+	LEFT JOIN folio_inventory.location__t il ON il.id = it.effectivelocationid
+	LEFT JOIN folio_inventory.loccampus__t lc ON lc.id = il.campus_id
+	LEFT JOIN folio_inventory.service_point__t sp ON sp.id = (fl.jsonb ->> 'checkoutServicePointId')::uuid
+	LEFT JOIN folio_inventory.statistical_code__t insc ON insc.id = (jsonb_path_query_first(ins.jsonb, '$.statisticalCodeIds[*]') #>> '{}')::uuid
 WHERE 
-    folio_derived.instance_statistical_codes.statistical_code_name = 'Hotspot'
-    AND (status = 'All' OR folio_derived.loans_items.loan_status = status)
-    AND folio_derived.loans_items.loan_date BETWEEN start_date AND end_date
-    AND	(service_point = 'All' OR folio_derived.loans_items.checkout_service_point_name = service_point)
-GROUP BY
-    folio_inventory.instance__t.id,
-    folio_inventory.instance__t.title,
-    folio_users.users__t.barcode,
-    folio_derived.locations_libraries.campus_name,
-    folio_derived.loans_items.loan_status,
-    folio_derived.users_groups.user_first_name,
-    folio_derived.users_groups.user_last_name,
-    folio_derived.loans_items.checkout_service_point_name,
-    folio_derived.loans_items.copy_number,
-    folio_derived.loans_items.item_status,
-    folio_derived.loans_items.loan_date,
-    folio_derived.loans_items.loan_return_date,
-    folio_derived.loans_items.loan_due_date,
-    folio_derived.loans_items.system_return_date,
-    folio_derived.loans_renewal_dates.loan_action_date,
-    folio_inventory.item__t.barcode,
-    folio_derived.holdings_ext.permanent_location_name,
-    folio_derived.item_ext.effective_location_name,
-    folio_derived.locations_libraries.location_name,
-    folio_inventory.item__t.id
+    insc.name = 'Hotspot'
+    AND (status = 'All' OR fl.jsonb ->> 'itemStatus' = status)
+    AND fl.jsonb ->> 'loanDate' BETWEEN start_date AND end_date
+    AND	(service_point = 'All' OR sp.name = service_point)
 $$
 LANGUAGE SQL
 STABLE

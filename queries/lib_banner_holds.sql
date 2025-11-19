527ae0be2af8851b79f201184108c3d1ecae84e6
rmartinez_8739 <rmartinez_8739@southtexascollege.edu>
2025-11-19 09:41:45 -0600

    update banner holds date output

--- queries/lib_banner_holds.sql
+++ queries/lib_banner_holds.sql
@@ -11,7 +11,7 @@ RETURNS TABLE(
     b_username text,
     c_patron_profile text,
     d_title text,
-    fee_date text,
+    fee_date timestamp without time zone,
     g_balance numeric
 )
 AS $$
@@ -20,7 +20,7 @@ SELECT
     u.username AS b_username,
     li.patron_group_name AS c_patron_profile,
     ihi.title AS d_title,
-    to_char(faa.transaction_date) AS fee_date,
+    date(faa.transaction_date) AS fee_date,
     faa.account_balance AS g_balance
 FROM
     folio_derived.feesfines_accounts_actions AS faa

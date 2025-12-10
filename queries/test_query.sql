42778b232d5dda6a365892d2351a024f1413ad1f
mresen19 <mresen19@southtexascollege.edu>
2025-12-10 14:04:32 -0600

    Updated test_query.sql

--- queries/test_query.sql
+++ queries/test_query.sql
@@ -18,38 +18,39 @@ RETURNS TABLE(
 AS $$
     with loans as (
         select
-            folio_circulation.loan__t.item_id as item_id,
+            folio_inventory.item.id as id,
             count(folio_circulation.loan__t.id) as checkouts
         from
             folio_circulation.loan__t
-        join folio_inventory.item__t on folio_inventory.item__t.id = folio_circulation.loan__t.item_id
-        group by folio_circulation.loan__t.item_id
+        join folio_inventory.item on folio_inventory.item.id = folio_circulation.loan__t.item_id
+        group by 
+            folio_inventory.item.id
     ),
     identifiers as (
         select
-            i.id as id,
+            folio_inventory.instance.id as id,
             string_agg(object ->> 'value', ', ') AS values
-        from folio_inventory.instance i
-        left join lateral jsonb_array_elements(i.jsonb -> 'identifiers') AS object on true
+        from folio_inventory.instance
+        left join lateral jsonb_array_elements(folio_inventory.instance.jsonb -> 'identifiers') AS object on true
         group by
-            i.id
+            folio_inventory.instance.id
     ),
     notes as (
         select
-            it.id as id,
+            folio_inventory.item.id as id,
             string_agg(object ->> 'note', ', ') filter (where folio_inventory.item_note_type__t.name = 'Staff Note') as values
-        from folio_inventory.item it
-        left join lateral jsonb_array_elements(it.jsonb -> 'notes') as object on true
+        from folio_inventory.item
+        left join lateral jsonb_array_elements(folio_inventory.item.jsonb -> 'notes') as object on true
         join folio_inventory.item_note_type__t on folio_inventory.item_note_type__t.id = (object ->> 'itemNoteTypeId')::uuid
         group by
-            it.id
+            folio_inventory.item.id
     ),
     codes as (
         select
-            it.id as id,
+            folio_inventory.item.id as id,
             folio_inventory.statistical_code__t.name as values
-        from folio_inventory.item it
-        left join lateral jsonb_array_elements_text(it.jsonb -> 'statisticalCodeIds') as object on true
+        from folio_inventory.item
+        left join lateral jsonb_array_elements_text(folio_inventory.item.jsonb -> 'statisticalCodeIds') as object on true
         left join folio_inventory.statistical_code__t on folio_inventory.statistical_code__t.id = object::uuid
     )
     select
@@ -61,10 +62,10 @@ AS $$
         notes.values as "F - Staff Notes",
         codes.values as "G - Statistical Codes"
     from folio_inventory.instance
-        join folio_inventory.holdings_record__t on folio_inventory.holdings_record__t.instance_id = folio_inventory.instance.id
-        join folio_inventory.item on folio_inventory.item.holdingsrecordid = folio_inventory.holdings_record__t.id
+        left join folio_inventory.holdings_record__t on folio_inventory.holdings_record__t.instance_id = folio_inventory.instance.id
+        left join folio_inventory.item on folio_inventory.item.holdingsrecordid = folio_inventory.holdings_record__t.id
         join folio_inventory.item__t on folio_inventory.item__t.id = folio_inventory.item.id
-        join loans on loans.item_id = folio_inventory.item.id
+        left join loans on loans.id = folio_inventory.item.id
         join identifiers on identifiers.id = folio_inventory.instance.id
         join notes on notes.id = folio_inventory.item.id
         join codes on codes.id = folio_inventory.item.id

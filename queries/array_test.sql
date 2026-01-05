6625e5763fadf6a453802b8389587d0e14f24509
mresen19 <mresen19@southtexascollege.edu>
2026-01-05 12:54:03 -0600

    Checking array values in report

---
+++ queries/array_test.sql
@@ -0,0 +1,20 @@
+--metadb:function array_test
+
+DROP FUNCTION IF EXISTS test;
+
+CREATE FUNCTION test(
+  item_barcode text[]
+)
+RETURNS TABLE(
+  instance_id uuid
+)
+AS $$
+    select distinct it.id as instance_id
+    from folio_inventory.instance__t it
+    left join folio_inventory.holdings_record__t hrt on (it.id = hrt.instance_id)
+    left join folio_inventory.item__t im on (im.holdings_record_id = hrt.id) 
+    where im.barcode in (item_barcode)
+$$
+LANGUAGE SQL
+STABLE
+PARALLEL SAFE;

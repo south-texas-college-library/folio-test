30705ece368d324d0ecec979b1ac59767b70141d
rmartinez_8739 <rmartinez_8739@southtexascollege.edu>
2026-01-13 08:12:37 -0600

    updated fees_fines/banner_hold to include the item_id

--- queries/lib_banner_holds.sql
+++ queries/lib_banner_holds.sql
@@ -13,8 +13,9 @@ RETURNS TABLE(
     d_username text,
     e_last_name text,
     f_first_name text,
-    g_item_title text,
-    h_fee_balance text
+    g_barcode text,
+    h_item_title text,
+    i_fee_balance text
 )
 AS $$
 SELECT
@@ -23,15 +24,18 @@ SELECT
     jsonb_extract_path_text(g.jsonb, 'group') AS c_patron_profile,
     jsonb_extract_path_text(u.jsonb, 'username') as d_username ,
     jsonb_extract_path_text(u.jsonb, 'personal', 'lastName') AS e_last_name ,
-    jsonb_extract_path_text(u.jsonb, 'personal', 'firstName') AS f_first_name ,    
-    jsonb_extract_path_text(a.jsonb, 'title') AS g_item_title,
-    jsonb_extract_path_text(a.jsonb, 'remaining') AS h_fee_balance
+    jsonb_extract_path_text(u.jsonb, 'personal', 'firstName') AS f_first_name ,
+    jsonb_extract_path_text(i.jsonb, 'barcode') as g_barcode,
+    jsonb_extract_path_text(a.jsonb, 'title') AS h_item_title,
+    jsonb_extract_path_text(a.jsonb, 'remaining') AS i_fee_balance
 FROM
     folio_feesfines.accounts AS a
     LEFT JOIN folio_users.users AS u
     ON u.id = jsonb_extract_path_text(a.jsonb, 'userId')::uuid
     LEFT JOIN folio_users.groups AS g
     ON g.id = jsonb_extract_path_text(u.jsonb, 'patronGroup')::uuid
+    LEFT JOIN folio_inventory.item i 
+    ON i.id = jsonb_extract_path_text(a.jsonb, 'itemId')::uuid
 WHERE
     (jsonb_extract_path_text(a.jsonb, 'remaining')::numeric >= min_fee AND jsonb_extract_path_text(a.jsonb, 'remaining')::numeric <= max_fee)
 ORDER BY

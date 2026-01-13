b85810bb48f56284509fdbbcfa35c3ae3d73f91e
mresen19 <mresen19@southtexascollege.edu>
2026-01-13 11:27:51 -0600

    Testing Link

--- queries/link.sql
+++ queries/link.sql
@@ -9,7 +9,7 @@ RETURNS TABLE(
   link text
 )
 AS $$
-    select concat('<a href="https://stc.folio.indexdata.com/inventory?filters=staffSuppress.false&qindex=items.barcode&query="', 140101, '"&segment=items&sort=title" target="_blank" rel="noopener">', barcode, '</a>')
+    select concat('<a href="https://stc.folio.indexdata.com/inventory?filters=staffSuppress.false&qindex=items.barcode&query=', barcode, '&segment=items&sort=title target=\"_blank\" rel=\"noopener\">', barcode, '</a>')
     from folio_inventory.item__t
     where barcode = item_barcode
 $$

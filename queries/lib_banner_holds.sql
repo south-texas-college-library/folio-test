ac6f67a02139f0c08c1c55fe68e6d8ff257dbb38
rmartinez_8739 <rmartinez_8739@southtexascollege.edu>
2026-01-20 10:12:54 -0600

    updated lib_banner_holds sql

--- queries/lib_banner_holds.sql
+++ queries/lib_banner_holds.sql
@@ -14,8 +14,9 @@ RETURNS TABLE(
     e_last_name text,
     f_first_name text,
     g_barcode text,
-    h_item_title text,
-    i_fee_balance text
+    h_barcode_url text
+    i_item_title text,
+    j_fee_balance text
 )
 AS $$
 SELECT
@@ -25,9 +26,10 @@ SELECT
     jsonb_extract_path_text(u.jsonb, 'username') as d_username ,
     jsonb_extract_path_text(u.jsonb, 'personal', 'lastName') AS e_last_name ,
     jsonb_extract_path_text(u.jsonb, 'personal', 'firstName') AS f_first_name ,
-    jsonb_extract_path_text(i.jsonb, 'barcode')||' - '||('https://stc-test.folio.indexdata.com/inventory/view/66fda4da-b953-59ba-b803-4b5e83cb8dc3/b2d06474-9d35-5f8b-a48e-bb834815b851/98428318-8e65-5b33-8542-e54b7199fb50?filters=staffSuppress.false&qindex=items.barcode&query='||jsonb_extract_path_text(i.jsonb, 'barcode')||'&segment=items&sort=title') as g_barcode,
-    jsonb_extract_path_text(a.jsonb, 'title') AS h_item_title ,
-    jsonb_extract_path_text(a.jsonb, 'remaining') AS i_fee_balance
+    jsonb_extract_path_text(i.jsonb, 'barcode') AS g_barcode ,
+    ('https://stc-test.folio.indexdata.com/inventory/view/66fda4da-b953-59ba-b803-4b5e83cb8dc3/b2d06474-9d35-5f8b-a48e-bb834815b851/98428318-8e65-5b33-8542-e54b7199fb50?filters=staffSuppress.false&qindex=items.barcode&query='||jsonb_extract_path_text(i.jsonb, 'barcode')||'&segment=items&sort=title') as h_barcode_url,
+    jsonb_extract_path_text(a.jsonb, 'title') AS i_item_title ,
+    jsonb_extract_path_text(a.jsonb, 'remaining') AS j_fee_balance
 FROM
     folio_feesfines.accounts AS a
     LEFT JOIN folio_users.users AS u
@@ -39,7 +41,7 @@ FROM
 WHERE
     (jsonb_extract_path_text(a.jsonb, 'remaining')::numeric >= min_fee AND jsonb_extract_path_text(a.jsonb, 'remaining')::numeric <= max_fee)
 ORDER BY
-    a_fee_date DESC, d_username ASC, i_fee_balance DESC
+    a_fee_date DESC, d_username ASC, j_fee_balance DESC
 $$
 LANGUAGE SQL
 STABLE

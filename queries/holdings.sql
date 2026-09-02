dff84504906a51764e42dcdc92125c7669c463ba
mresen19 <mresen19@southtexascollege.edu>
2026-09-02 12:12:12 -0500

    Updated holdings.sql

--- queries/holdings.sql
+++ queries/holdings.sql
@@ -5,13 +5,13 @@ DROP FUNCTION IF EXISTS holdings;
 CREATE FUNCTION holdings()
 
 RETURNS TABLE(
-    "Instance ID" TEXT,
-    "HR ID" TEXT
+    "Instance ID" UUID,
+    "HR ID" UUID
 )
 AS $$
     select 
-        ins.id::text,
-        hr.id::text
+        ins.id,
+        hr.id
     from folio_inventory.instance__t ins
     join folio_inventory.holdings_record__t hr on hr.instance_id = ins.id
 $$

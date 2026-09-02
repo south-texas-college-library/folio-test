e7371ab6e33d4b2194f79dc6f11681fb239b608d
mresen19 <mresen19@southtexascollege.edu>
2026-09-02 12:05:53 -0500

    Updated test report.

--- queries/holdings.sql
+++ queries/holdings.sql
@@ -10,8 +10,8 @@ RETURNS TABLE(
 )
 AS $$
     select 
-        ins.id,
-        hr.id
+        ins.id::text,
+        hr.id::text
     from folio_inventory.instance__t ins
     join folio_inventory.holdings_record__t hr on hr.instance_id = ins.id
 $$

b2bab12bf3386ffe052dd9770f2121e6e581d17e
mresen19 <mresen19@southtexascollege.edu>
2026-09-02 13:03:10 -0500

    Updated test reports.

--- queries/items.sql
+++ queries/items.sql
@@ -5,10 +5,14 @@ DROP FUNCTION IF EXISTS items;
 CREATE FUNCTION items()
 
 RETURNS TABLE(
+    "Instance ID" TEXT,
+    "HR ID" TEXT,
     "Item ID" TEXT
 )
 AS $$
     select
+        ins.id,
+        hr.id,
         it.id
     from folio_inventory.instance__t ins
     join folio_inventory.holdings_record__t hr on hr.instance_id = ins.id

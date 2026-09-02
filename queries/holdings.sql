120de9f2f672cc546860e7f6321f106b9c9b60cc
mresen19 <mresen19@southtexascollege.edu>
2026-09-02 10:49:25 -0500

    Added columns to test reports to check if it breaks limits.

--- queries/holdings.sql
+++ queries/holdings.sql
@@ -5,11 +5,39 @@ DROP FUNCTION IF EXISTS holdings;
 CREATE FUNCTION holdings()
 
 RETURNS TABLE(
-    "ID" TEXT
+    "Instance ID" TEXT,
+    "Title" TEXT,
+    "Discovery Suppress" TEXT,
+    "Staff Suppress" TEXT,
+    "Deleted" TEXT,
+    "Index Title" TEXT,
+    "Cataloged Date" TEXT,
+    "Status ID" TEXT,
+    "HR ID" TEXT,
+    "Call Number" TEXT,
+    "Discovery Suppress" TEXT,
+    "Permanent Location" TEXT,
+    "Temporary Location" TEXT,
+    "Effective Location" TEXT,
+    "Copy Number" TEXT
 )
 AS $$
     select 
-        hr.id
+        ins.id,
+        ins.TITLE,
+        ins.DISCOVERY_SUPPRESS,
+        ins.STAFF_SUPPRESS,
+        ins.DELETED,
+        ins.INDEX_TITLE,
+        ins.CATALOGED_DATE,
+        ins.STATUS_ID,
+        hr.id,
+        hr.call_number,
+        hr.DISCOVERY_SUPPRESS,
+        hr.PERMANENT_LOCATION_ID,
+        hr.TEMPORARY_LOCATION_ID,
+        hr.EFFECTIVE_LOCATION_ID,
+        hr.COPY_NUMBER
     from folio_inventory.instance__t ins
     join folio_inventory.holdings_record__t hr on hr.instance_id = ins.id
 $$
--- queries/items.sql
+++ queries/items.sql
@@ -5,11 +5,63 @@ DROP FUNCTION IF EXISTS items;
 CREATE FUNCTION items()
 
 RETURNS TABLE(
-    "ID" TEXT
+    "Instance ID" TEXT,
+    "Title" TEXT,
+    "Discovery Suppress" TEXT,
+    "Staff Suppress" TEXT,
+    "Deleted" TEXT,
+    "Index Title" TEXT,
+    "Cataloged Date" TEXT,
+    "Status ID" TEXT,
+    "HR ID" TEXT,
+    "Call Number" TEXT,
+    "Discovery Suppress" TEXT,
+    "Permanent Location" TEXT,
+    "Temporary Location" TEXT,
+    "Effective Location" TEXT,
+    "Copy Number" TEXT,
+    "Item ID" TEXT,
+    "Item Barcode" TEXT,
+    "Discovery Suppress" TEXT,
+    "Chronology" TEXT,
+    "Enumeration" TEXT,
+    "Item Identifier" TEXT,
+    "Material Type ID" TEXT,
+    "Missing Pieces" TEXT,
+    "Permanent Location ID" TEXT,
+    "Temporary Location ID" TEXT,
+    "Effective Location ID" TEXT,
+    "Volume" TEXT,
 )
 AS $$
-    select 
-        it.id
+    select
+        ins.id,
+        ins.TITLE,
+        ins.DISCOVERY_SUPPRESS,
+        ins.STAFF_SUPPRESS,
+        ins.DELETED,
+        ins.INDEX_TITLE,
+        ins.CATALOGED_DATE,
+        ins.STATUS_ID,
+        hr.id,
+        hr.call_number,
+        hr.DISCOVERY_SUPPRESS,
+        hr.PERMANENT_LOCATION_ID,
+        hr.TEMPORARY_LOCATION_ID,
+        hr.EFFECTIVE_LOCATION_ID,
+        hr.COPY_NUMBER,
+        it.id,
+        it.barcode,
+        it.DISCOVERY_SUPPRESS,
+        it.CHRONOLOGY,
+        it.ENUMERATION,
+        it.ITEM_IDENTIFIER,
+        it.MATERIAL_TYPE_ID,
+        it.MISSING_PIECES,
+        it.PERMANENT_LOCATION_ID,
+        it.TEMPORARY_LOCATION_ID,
+        it.EFFECTIVE_LOCATION_ID,
+        it.VOLUME
     from folio_inventory.instance__t ins
     join folio_inventory.holdings_record__t hr on hr.instance_id = ins.id
     join folio_inventory.item__t it on it.HOLDINGS_RECORD_ID = hr.id

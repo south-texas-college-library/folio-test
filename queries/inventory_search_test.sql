79802eb48dd6427c3dc413a1881198678cda7876
mresen19 <mresen19@southtexascollege.edu>
2026-09-02 14:17:35 -0500

    Updated inventory_search_test.sql

--- queries/inventory_search_test.sql
+++ queries/inventory_search_test.sql
@@ -167,7 +167,7 @@ AS $$
         AND (sub_type = 'All' OR subtype.name = sub_type)
     ORDER BY
         hr.call_number, jsonb_extract_path_text(it.jsonb, 'barcode')
-    LIMIT 50000
+    LIMIT 25000
 $$
 LANGUAGE SQL
 STABLE
